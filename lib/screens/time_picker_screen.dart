a1d94fb775aa9e1524f2f7c00cd84699c0ddcae1
Seboostian02 <antique_sebi@yahoo.ro>
2024-10-08 15:34:19 +0300

    comment 'create event' app bar

--- lib/screens/time_picker_screen.dart
+++ lib/screens/time_picker_screen.dart
@@ -7,7 +7,7 @@ class TimePickerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Create Event')),
+      // appBar: AppBar(title: const Text('Create Event')),
       body: const EventForm(),
     );
   }
