57fef2c93a33928c89da1e7e4c08ee9f457f85fe
Seboostian02 <antique_sebi@yahoo.ro>
2024-10-15 21:15:17 +0300

    -

--- lib/widgets/location_search_bar.dart
+++ lib/widgets/location_search_bar.dart
@@ -1,40 +1,40 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_place/google_place.dart';
+import 'package:flutter/material.dart';
+import 'package:google_place/google_place.dart';
 
-// class LocationSearchBar extends StatelessWidget {
-//   final Function(String) onSearch;
-//   final List<AutocompletePrediction> predictions;
-//   final Function(String) onPredictionSelected;
+class LocationSearchBar extends StatelessWidget {
+  final Function(String) onSearch;
+  final List<AutocompletePrediction> predictions;
+  final Function(String) onPredictionSelected;
 
-//   LocationSearchBar({
-//     required this.onSearch,
-//     required this.predictions,
-//     required this.onPredictionSelected,
-//   });
+  LocationSearchBar({
+    required this.onSearch,
+    required this.predictions,
+    required this.onPredictionSelected,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: [
-//         TextField(
-//           onChanged: (value) => onSearch(value),
-//           decoration: InputDecoration(
-//             hintText: "Search location",
-//             filled: true,
-//             fillColor: Colors.white,
-//             border: OutlineInputBorder(
-//               borderRadius: BorderRadius.circular(8),
-//               borderSide: BorderSide.none,
-//             ),
-//           ),
-//         ),
-//         ...predictions.map(
-//           (p) => ListTile(
-//             title: Text(p.description ?? ''),
-//             onTap: () => onPredictionSelected(p.placeId!),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          onChanged: (value) => onSearch(value),
+          decoration: InputDecoration(
+            hintText: "Search location",
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
+        ...predictions.map(
+          (p) => ListTile(
+            title: Text(p.description ?? ''),
+            onTap: () => onPredictionSelected(p.placeId!),
+          ),
+        ),
+      ],
+    );
+  }
+}
