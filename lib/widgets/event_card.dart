6cfe78666af472f1e7b3be6bad8c5c762868852e
Seboostian02 <antique_sebi@yahoo.ro>
2024-10-19 17:08:42 +0300

    add map on location press

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -1,5 +1,9 @@
+import 'package:calendar/screens/event_view_on_map.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:googleapis/calendar/v3.dart' as calendar;
 
 class EventCard extends StatelessWidget {
   final String title;
@@ -31,6 +35,16 @@ class EventCard extends StatelessWidget {
     final randomIndex = Random().nextInt(colors.length);
     final backgroundColor = colors[randomIndex];
 
+    final startDateTime = DateTime.parse(startTime);
+    final endDateTime = DateTime.parse(endTime);
+    final String startHour =
+        "${startDateTime.hour}:${startDateTime.minute.toString().padLeft(2, '0')}";
+    final String endHour =
+        "${endDateTime.hour}:${endDateTime.minute.toString().padLeft(2, '0')}";
+
+    final String formattedDate =
+        DateFormat('EEEE, d MMMM yyyy').format(startDateTime);
+
     return Card(
       elevation: 4,
       margin: const EdgeInsets.symmetric(vertical: 8.0),
@@ -42,26 +56,46 @@ class EventCard extends StatelessWidget {
           children: [
             Text(
               title,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              'Location: $location',
-              style: const TextStyle(fontSize: 16),
+              style: const TextStyle(fontSize: 23, fontWeight: FontWeight.w800),
             ),
             const SizedBox(height: 4),
             Text(
               description,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 4),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EventView(
+                      events: [],
+                    ),
+                  ),
+                );
+              },
+              child: Text(
+                'Location: $location',
+                style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline),
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              'Date: $formattedDate',
               style: const TextStyle(fontSize: 16),
             ),
             const SizedBox(height: 4),
             Text(
-              'Starts at: $startTime',
+              'Starts at: $startHour',
               style: const TextStyle(fontSize: 16),
             ),
             const SizedBox(height: 4),
             Text(
-              'Ends at: $endTime',
+              'Ends at: $endHour',
               style: const TextStyle(fontSize: 16),
             ),
           ],
