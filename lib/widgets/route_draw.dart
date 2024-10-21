abaa557b67c3c5a8d81b9627a8cdea207e731bd6
Seboostian02 <antique_sebi@yahoo.ro>
2024-10-21 09:04:08 +0300

    actually draw the route on map

--- lib/widgets/route_draw.dart
+++ lib/widgets/route_draw.dart
@@ -3,18 +3,63 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_line_editor/flutter_map_line_editor.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:calendar/widgets/zoom_controls.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class RouteDrawer extends StatelessWidget {
+class RouteDrawer extends StatefulWidget {
   final LatLng currentLocation;
   final LatLng destination;
-  final MapController mapController;
 
   RouteDrawer({
     Key? key,
     required this.currentLocation,
     required this.destination,
-  })  : mapController = MapController(),
-        super(key: key);
+  }) : super(key: key);
+
+  @override
+  _RouteDrawerState createState() => _RouteDrawerState();
+}
+
+class _RouteDrawerState extends State<RouteDrawer> {
+  late MapController mapController;
+  String selectedTransportMode = 'Bus';
+  List<LatLng> routePoints = [];
+
+  @override
+  void initState() {
+    super.initState();
+    mapController = MapController();
+    _fetchRoute();
+  }
+
+  Future<void> _fetchRoute() async {
+    final mode = selectedTransportMode.toLowerCase();
+    final url =
+        'https://router.project-osrm.org/route/v1/$mode/${widget.currentLocation.longitude},${widget.currentLocation.latitude};${widget.destination.longitude},${widget.destination.latitude}?geometries=geojson';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        routePoints = (data['routes'][0]['geometry']['coordinates'] as List)
+            .map((coord) => LatLng(coord[1], coord[0]))
+            .toList();
+      });
+    } else {
+      print('Failed to fetch route: ${response.statusCode}');
+    }
+  }
+
+  List<Polyline> _generateRoutes() {
+    return [
+      Polyline(
+        points: routePoints,
+        strokeWidth: 4.0,
+        color: Colors.blue,
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +68,7 @@ class RouteDrawer extends StatelessWidget {
         FlutterMap(
           mapController: mapController,
           options: MapOptions(
-            center: currentLocation,
+            center: widget.currentLocation,
             zoom: 13.0,
           ),
           children: [
@@ -31,18 +76,72 @@ class RouteDrawer extends StatelessWidget {
               urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
               subdomains: ['a', 'b', 'c'],
             ),
-            PolylineLayer(
-              polylines: [
-                Polyline(
-                  points: [currentLocation, destination],
-                  strokeWidth: 4.0,
-                  color: Colors.blue,
+            MarkerLayer(
+              markers: [
+                Marker(
+                  point: widget.currentLocation,
+                  builder: (context) => const Icon(
+                    Icons.my_location,
+                    color: Colors.blue,
+                    size: 40.0,
+                  ),
+                ),
+                Marker(
+                  point: widget.destination,
+                  builder: (context) => const Icon(
+                    Icons.location_on,
+                    color: Colors.red,
+                    size: 40.0,
+                  ),
                 ),
               ],
+            ),
+            PolylineLayer(
+              polylines: _generateRoutes(),
             ),
           ],
         ),
         ZoomControls(mapController: mapController),
+        Positioned(
+          bottom: 20,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 4.0,
+                    spreadRadius: 2.0,
+                  ),
+                ],
+              ),
+              child: DropdownButton<String>(
+                value: selectedTransportMode,
+                underline: Container(),
+                dropdownColor: Colors.white,
+                style: const TextStyle(color: Colors.black), // Stil text
+                items: <String>['Walking', 'Bus', 'Cycling', 'Car']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedTransportMode = newValue!;
+                    _fetchRoute();
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
         Positioned(
           top: 50,
           left: 20,
