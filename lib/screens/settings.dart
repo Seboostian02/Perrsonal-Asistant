38d61857b14d36c1583e0758c9c7f1a57cb4974b
Seboostian02 <antique_sebi@yahoo.ro>
2024-10-22 17:59:35 +0300

    add settings tab

---
+++ lib/screens/settings.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import '../services/auth_provider.dart';
+
+class SettingsList extends StatelessWidget {
+  final AuthProvider
+      authProvider; // Trebuie să primești AuthProvider pentru verificare
+
+  const SettingsList({Key? key, required this.authProvider}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          ListTile(
+            leading: const Icon(Icons.account_circle),
+            title: const Text('Profile'),
+            onTap: () {
+              print("profile -----------");
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: const Text('Notifications'),
+            onTap: () {
+              print("notifications -----------");
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.lock),
+            title: const Text('Privacy'),
+            onTap: () {
+              print("privacy -----------");
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.help),
+            title: const Text('Help & Support'),
+            onTap: () {
+              print("HEEEELP -----------");
+            },
+          ),
+          const Divider(),
+          const SizedBox(height: 20),
+          if (authProvider.isLoggedIn)
+            ElevatedButton(
+              onPressed: authProvider.signOut,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red.shade700,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              ),
+              child:
+                  const Text('Log Out', style: TextStyle(color: Colors.white)),
+            ),
+        ],
+      ),
+    );
+  }
+}
