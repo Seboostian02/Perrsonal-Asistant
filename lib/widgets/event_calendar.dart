3c2bcb37bcfbb09f90b2cead83d07885bb1e4655
Seboostian02 <antique_sebi@yahoo.ro>
2024-10-29 19:25:40 +0200

    added calendar design

---
+++ lib/widgets/event_calendar.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:googleapis/calendar/v3.dart' as calendar;
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
+import '../widgets/event_card.dart';
+
+class EventCalendar extends StatefulWidget {
+  final List<calendar.Event> events;
+  final VoidCallback onRefresh;
+
+  const EventCalendar({
+    Key? key,
+    required this.events,
+    required this.onRefresh,
+  }) : super(key: key);
+
+  @override
+  _EventCalendarState createState() => _EventCalendarState();
+}
+
+class _EventCalendarState extends State<EventCalendar> {
+  late DateTime _selectedDay;
+  late Map<DateTime, List<calendar.Event>> _eventMap;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = DateTime.now().toLocal();
+    _eventMap = _groupEventsByDate();
+  }
+
+  Map<DateTime, List<calendar.Event>> _groupEventsByDate() {
+    Map<DateTime, List<calendar.Event>> eventMap = {};
+    for (var event in widget.events) {
+      final eventDate = event.start?.dateTime?.toLocal();
+      if (eventDate != null) {
+        final date = DateTime(eventDate.year, eventDate.month, eventDate.day);
+        if (eventMap[date] == null) {
+          eventMap[date] = [];
+        }
+        eventMap[date]!.add(event);
+      }
+    }
+    return eventMap;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final normalizedSelectedDay =
+        DateTime(_selectedDay.year, _selectedDay.month, _selectedDay.day);
+    final eventsForSelectedDay = _eventMap[normalizedSelectedDay] ?? [];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Event Calendar'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TableCalendar(
+                    firstDay: DateTime.utc(2020, 1, 1),
+                    lastDay: DateTime.utc(2030, 12, 31),
+                    focusedDay: _selectedDay,
+                    selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        _selectedDay = selectedDay.toLocal();
+                      });
+                    },
+                    calendarFormat: CalendarFormat.month,
+                    eventLoader: (day) => _eventMap[day] ?? [],
+                    holidayPredicate: (day) => _eventMap.containsKey(
+                      DateTime(day.year, day.month, day.day),
+                    ),
+                    calendarStyle: const CalendarStyle(
+                      todayDecoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedDecoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                      holidayDecoration: BoxDecoration(
+                        color: Color(0xFFE1BEE7),
+                        shape: BoxShape.circle,
+                      ),
+                      holidayTextStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      cellMargin: EdgeInsets.all(4.0),
+                      markersMaxCount: 1,
+                      markersAlignment: Alignment.bottomCenter,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    constraints: BoxConstraints(
+                      minHeight: 200,
+                      maxHeight: MediaQuery.of(context).size.height * 0.5,
+                    ),
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey.shade50,
+                      borderRadius: BorderRadius.circular(8.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          blurRadius: 4,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Text(
+                            'Events for ${DateFormat('d MMMM yyyy').format(_selectedDay)}',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        if (eventsForSelectedDay.isNotEmpty) ...[
+                          const Center(
+                            child: Text(
+                              'You have events today!',
+                              style: TextStyle(
+                                color: Colors.green,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: eventsForSelectedDay.length,
+                            itemBuilder: (context, index) {
+                              final event = eventsForSelectedDay[index];
+                              return EventCard(
+                                event: event,
+                                showLocation: true,
+                                onDelete: widget.onRefresh,
+                              );
+                            },
+                          ),
+                        ] else
+                          const Center(
+                            child: Text(
+                              "No events for today.",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black54,
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
