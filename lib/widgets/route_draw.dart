7fb4e2ee3755bdd3060e5ed541b5b227f6cdac9e
Seboostian02 <antique_sebi@yahoo.ro>
2024-11-07 20:12:41 +0200

    -

--- lib/widgets/route_draw.dart
+++ lib/widgets/route_draw.dart
@@ -3,7 +3,6 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 import 'package:calendar/env/env.dart';
 import 'package:calendar/widgets/zoom_controls.dart';
 
@@ -25,12 +24,11 @@ class _RouteDrawerState extends State<RouteDrawer> {
   late MapController mapController;
   String selectedTransportMode = 'Driving';
   List<LatLng> routePoints = [];
-  bool isTrafficLayerVisible = false;
 
   final Map<String, String> transportModes = {
-    'Pedestrian': 'pedestrian',
-    'Bike': 'bicycle',
-    'Driving': 'car'
+    'Foot': 'foot-walking',
+    'Bike': 'cycling-regular',
+    'Driving': 'driving-hgv',
   };
 
   @override
@@ -41,39 +39,53 @@ class _RouteDrawerState extends State<RouteDrawer> {
   }
 
   Future<void> _fetchRoute() async {
-    final String? apiKey = Env.tomTomKey;
-    final mode = transportModes[selectedTransportMode] ?? 'car';
+    final String? apiKey = Env.opsKey;
 
-    if (widget.currentLocation == widget.destination) {
+    if (widget.currentLocation.latitude == widget.destination.latitude &&
+        widget.currentLocation.longitude == widget.destination.longitude) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content: Text('Start and destination points are the same!')),
+        SnackBar(
+          content: Text('Punctele de start și destinație sunt identice!'),
+        ),
       );
       return;
     }
 
-    final url = 'https://api.tomtom.com/routing/1/calculateRoute/'
-        '${widget.currentLocation.latitude},${widget.currentLocation.longitude}:'
-        '${widget.destination.latitude},${widget.destination.longitude}/json?'
-        'travelMode=$mode&traffic=true&key=$apiKey';
+    final mode = transportModes[selectedTransportMode];
+
+    final url =
+        'https://api.openrouteservice.org/v2/directions/$mode?api_key=$apiKey&start=${widget.currentLocation.longitude},${widget.currentLocation.latitude}&end=${widget.destination.longitude},${widget.destination.latitude}';
 
     try {
-      final response = await http.get(Uri.parse(url));
+      final response = await http.get(
+        Uri.parse(url),
+        headers: {
+          'Accept':
+              'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
+        },
+      );
 
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
-        final coordinates = data['routes'][0]['legs'][0]['points'];
 
-        setState(() {
-          routePoints = coordinates
-              .map<LatLng>(
-                  (point) => LatLng(point['latitude'], point['longitude']))
-              .toList();
-          isTrafficLayerVisible =
-              (mode == 'car'); // Show traffic for driving only
-        });
-      } else {
-        print('Failed to fetch route: ${response.statusCode}');
+        if (data['features'] != null && data['features'].isNotEmpty) {
+          final steps =
+              data['features'][0]['properties']['segments'][0]['steps'];
+
+          List<int> waypoints = [];
+          for (var step in steps) {
+            if (step['way_points'] != null) {
+              waypoints.addAll(List<int>.from(step['way_points']));
+            }
+          }
+
+          setState(() {
+            routePoints =
+                (data['features'][0]['geometry']['coordinates'] as List)
+                    .map((coord) => LatLng(coord[1], coord[0]))
+                    .toList();
+          });
+        }
       }
     } catch (e) {
       print('Error fetching route: $e');
@@ -90,19 +102,6 @@ class _RouteDrawerState extends State<RouteDrawer> {
     ];
   }
 
-  String createMapUrl() {
-    String baseURL = 'https://api.tomtom.com';
-    String versionNumber = '1';
-    String resourceVersion = '20.3.4-6'; // Alege versiunea dorită
-    String mapStyle = 'basic_main'; // Stilul hărții
-    String trafficIncidentStyle = 'incidents_day'; // Stilul incidentelor
-    String trafficFlowStyle = 'flow_absolute'; // Stilul fluxului de trafic
-    String poiStyle = 'poi_main'; // Stilul punctelor de interes (POI)
-    print(
-        "$baseURL/style/$versionNumber/style/$resourceVersion?key=${Env.tomTomKey}&map=$mapStyle&traffic_incidents=$trafficIncidentStyle&traffic_flow=$trafficFlowStyle&poi=$poiStyle");
-    return '$baseURL/style/$versionNumber/style/$resourceVersion?key=${Env.tomTomKey}&map=$mapStyle&traffic_incidents=$trafficIncidentStyle&traffic_flow=$trafficFlowStyle&poi=$poiStyle';
-  }
-
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -114,32 +113,11 @@ class _RouteDrawerState extends State<RouteDrawer> {
             zoom: 13.0,
           ),
           children: [
-            // Base map layer
             TileLayer(
               urlTemplate:
-                  'https://api.tomtom.com/map/1/tile/basic/main/{z}/{x}/{y}.png?key=${Env.tomTomKey}',
+                  'https://api.tomtom.com/map/tiles/road/maps?key=${Env.tomTomKey}&tileSize=256&zoom={z}&x={x}&y={y}',
               subdomains: ['a', 'b', 'c'],
-              // Poți încerca un alt provider de tile-uri
             ),
-            TileLayer(
-              urlTemplate:
-                  'https://api.tomtom.com/traffic/map/4/tile/flow/relative0/{z}/{x}/{y}.png?key=${Env.tomTomKey}',
-              subdomains: ['a', 'b', 'c'],
-              // tileProvider: NonCachingNetworkTileProvider(),
-            ),
-            // Traffic overlay layer with higher zoom and transparency
-            // if (isTrafficLayerVisible)
-            //   ColorFiltered(
-            //     colorFilter: ColorFilter.mode(
-            //       Colors.black.withOpacity(0.1), // Adjust opacity here
-            //       BlendMode.srcATop,
-            //     ),
-            //     child: TileLayer(
-            //       urlTemplate:
-            //           'https://api.tomtom.com/traffic/map/4/tile/flow/relative0/{z}/{x}/{y}.png?key=${Env.tomTomKey}',
-            //     ),
-            //   ),
-            // Marker for current location and destination
             MarkerLayer(
               markers: [
                 Marker(
@@ -160,42 +138,12 @@ class _RouteDrawerState extends State<RouteDrawer> {
                 ),
               ],
             ),
-            // Route line
             PolylineLayer(
               polylines: _generateRoutes(),
             ),
           ],
         ),
-        // Zoom controls
         ZoomControls(mapController: mapController),
-        // Back button
-        Positioned(
-          top: 50,
-          left: 20,
-          child: Material(
-            elevation: 6,
-            shape: const CircleBorder(),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: Container(
-                width: 56,
-                height: 56,
-                decoration: const BoxDecoration(
-                  color: Colors.deepPurple,
-                  shape: BoxShape.circle,
-                ),
-                child: const Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                  size: 30,
-                ),
-              ),
-            ),
-          ),
-        ),
-        // Dropdown for selecting travel mode
         Positioned(
           bottom: 20,
           left: 0,
@@ -234,6 +182,32 @@ class _RouteDrawerState extends State<RouteDrawer> {
                     });
                   }
                 },
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 50,
+          left: 20,
+          child: Material(
+            elevation: 6,
+            shape: const CircleBorder(),
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurple,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 30,
+                ),
               ),
             ),
           ),
