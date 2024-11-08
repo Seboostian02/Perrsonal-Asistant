4e36d148af4f65f2440b73f334beceab1cd9e232
Seboostian02 <antique_sebi@yahoo.ro>
2024-11-08 12:30:22 +0200

    -

--- lib/widgets/route_draw.dart
+++ lib/widgets/route_draw.dart
@@ -1,10 +1,11 @@
-import 'package:calendar/env/env.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:calendar/widgets/zoom_controls.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:calendar/env/env.dart';
+import 'package:calendar/widgets/zoom_controls.dart';
 
 class RouteDrawer extends StatefulWidget {
   final LatLng currentLocation;
@@ -24,10 +25,14 @@ class _RouteDrawerState extends State<RouteDrawer> {
   late MapController mapController;
   String selectedTransportMode = 'Driving';
   List<LatLng> routePoints = [];
+  bool isTrafficLayerVisible = false;
+  List<Marker> incidentMarkers = [];
+  List<Polyline> trafficFlowPolylines = [];
+
   final Map<String, String> transportModes = {
-    'Foot': 'foot-walking',
-    'Bike': 'cycling-regular',
-    'Driving': 'driving-hgv',
+    'Walking': 'pedestrian',
+    'Bike': 'bicycle',
+    'Driving': 'car'
   };
 
   @override
@@ -35,75 +40,136 @@ class _RouteDrawerState extends State<RouteDrawer> {
     super.initState();
     mapController = MapController();
     _fetchRoute();
+    _fetchTrafficIncidents();
+    _fetchTrafficFlow();
   }
 
   Future<void> _fetchRoute() async {
-    final String? apiKey = Env.opsKey; // OpenRouteService key
+    final String? apiKey = Env.tomTomKey;
+    final mode = transportModes[selectedTransportMode] ?? 'car';
 
-    if (widget.currentLocation.latitude == widget.destination.latitude &&
-        widget.currentLocation.longitude == widget.destination.longitude) {
+    if (widget.currentLocation == widget.destination) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Punctele de start și destinație sunt identice!'),
-        ),
+        const SnackBar(
+            content: Text('Start and destination points are the same!')),
       );
       return;
     }
 
-    final mode = transportModes[selectedTransportMode];
+    final url = 'https://api.tomtom.com/routing/1/calculateRoute/'
+        '${widget.currentLocation.latitude},${widget.currentLocation.longitude}:'
+        '${widget.destination.latitude},${widget.destination.longitude}/json?'
+        'travelMode=$mode&traffic=true&key=$apiKey';
 
-    final routeUrl =
-        'https://api.openrouteservice.org/v2/directions/$mode?api_key=$apiKey&start=${widget.currentLocation.longitude},${widget.currentLocation.latitude}&end=${widget.destination.longitude},${widget.destination.latitude}';
-
-    // HERE Traffic API URL
-    String your_api_key = "JExNxMop8Ape0rFQ54bzD_FUSih2_QKSGuM3cNT-m2k";
-
-    final trafficUrl =
-        'https://traffic.ls.hereapi.com/traffic/6.2/flow.json?apiKey=${your_api_key}&bbox=${widget.currentLocation.latitude},${widget.currentLocation.longitude},${widget.destination.latitude},${widget.destination.longitude}'; //traffic.ls.hereapi.com/traffic/6.2/flow.json?apiKey=JExNxMop8Ape0rFQ54bzD_FUSih2_QKSGuM3cNT-m2k&bbox=${widget.currentLocation.latitude},${widget.currentLocation.longitude},${widget.destination.latitude},${widget.destination.longitude}';
-    print("traffic url----------");
-    print(trafficUrl);
     try {
-      // Request route data
-      final routeResponse = await http.get(
-        Uri.parse(routeUrl),
-        headers: {
-          'Accept':
-              'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
-        },
-      );
+      final response = await http.get(Uri.parse(url));
 
-      if (routeResponse.statusCode == 200) {
-        final routeData = json.decode(routeResponse.body);
-        if (routeData['features'] != null && routeData['features'].isNotEmpty) {
-          final coordinates =
-              (routeData['features'][0]['geometry']['coordinates'] as List)
-                  .map((coord) => LatLng(coord[1], coord[0]))
-                  .toList();
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final coordinates = data['routes'][0]['legs'][0]['points'];
 
-          setState(() {
-            routePoints = coordinates;
-          });
-        }
+        setState(() {
+          routePoints = coordinates
+              .map<LatLng>(
+                  (point) => LatLng(point['latitude'], point['longitude']))
+              .toList();
+          isTrafficLayerVisible = (mode == 'car');
+        });
       } else {
-        print('Failed to fetch route data: ${routeResponse.statusCode}');
-      }
-
-      // Request traffic data from HERE API
-      final trafficResponse = await http.get(Uri.parse(trafficUrl));
-      if (trafficResponse.statusCode == 200) {
-        final trafficData = json.decode(trafficResponse.body);
-        print('Traffic Data: $trafficData');
-
-        // Procesăm datele de trafic pentru a vizualiza aglomerația, dacă este necesar
-        if (trafficData['flow'] != null) {
-          // Puteți să utilizați datele de trafic pentru a vizualiza congestia pe traseu
-          // De exemplu, actualizează traseul sau colorează traseul pe baza condițiilor de trafic
-        }
-      } else {
-        print('Failed to fetch traffic data: ${trafficResponse.statusCode}');
+        print('Failed to fetch route: ${response.statusCode}');
       }
     } catch (e) {
-      print('Error fetching route or traffic data: $e');
+      print('Error fetching route: $e');
+    }
+  }
+
+  Future<void> _fetchTrafficIncidents() async {
+    final String? apiKey = Env.tomTomKey;
+
+    final String bbox =
+        '4.8854592519716675,52.36934334773164,4.897883244144765,52.37496348620152';
+    final String fields =
+        '{incidents{type,geometry{type,coordinates},properties{iconCategory}}}';
+
+    final url =
+        'https://api.tomtom.com/traffic/services/5/incidentDetails?key=$apiKey&bbox=$bbox&fields=$fields&language=en-GB&t=1111&timeValidityFilter=present';
+    print("url--------");
+    print(url);
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        List<Marker> markers = [];
+        for (var incident in data['incidents']) {
+          final coordinates = incident['geometry']['coordinates'];
+
+          if (coordinates is List && coordinates.isNotEmpty) {
+            final incidentLon = coordinates[0][0];
+            final incidentLat = coordinates[0][1];
+
+            markers.add(Marker(
+              point: LatLng(incidentLat, incidentLon),
+              builder: (context) =>
+                  Icon(Icons.warning, color: Colors.red, size: 40.0),
+            ));
+          }
+        }
+
+        setState(() {
+          incidentMarkers = markers;
+        });
+      } else {
+        print('Failed to fetch traffic incidents: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching incidents: $e');
+    }
+  }
+
+  Future<void> _fetchTrafficFlow() async {
+    final String? apiKey = Env.tomTomKey;
+
+    // Bounding box for the area of interest (you can adjust this dynamically)
+    final String bbox =
+        '4.8854592519716675,52.36934334773164,4.897883244144765,52.37496348620152';
+
+    // Fetching traffic flow tiles as polylines
+    final url =
+        'https://api.tomtom.com/traffic/services/5/trafficFlowTiles?key=$apiKey&bbox=$bbox';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        List<Polyline> trafficPolylines = [];
+        for (var flow in data['flowData']) {
+          final coordinates = flow['geometry']['coordinates'];
+
+          // Converting coordinates to LatLng
+          List<LatLng> flowPoints = coordinates
+              .map<LatLng>((point) =>
+                  LatLng(point[1], point[0])) // reverse lat/lng order
+              .toList();
+
+          trafficPolylines.add(Polyline(
+            points: flowPoints,
+            strokeWidth: 3.0,
+            color: Colors.orange,
+          ));
+        }
+
+        setState(() {
+          trafficFlowPolylines = trafficPolylines;
+        });
+      } else {
+        print('Failed to fetch traffic flow: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching traffic flow: $e');
     }
   }
 
@@ -130,35 +196,61 @@ class _RouteDrawerState extends State<RouteDrawer> {
           children: [
             TileLayer(
               urlTemplate:
-                  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', // Use OpenStreetMap tiles
+                  'https://api.tomtom.com/map/1/tile/basic/main/{z}/{x}/{y}.png?key=${Env.tomTomKey}',
               subdomains: ['a', 'b', 'c'],
             ),
+            if (isTrafficLayerVisible)
+              TileLayer(
+                urlTemplate:
+                    'https://api.tomtom.com/traffic/map/4/tile/flow/relative0/{z}/{x}/{y}.png?key=${Env.tomTomKey}',
+              ),
             MarkerLayer(
               markers: [
                 Marker(
                   point: widget.currentLocation,
-                  builder: (context) => const Icon(
-                    Icons.my_location,
-                    color: Colors.blue,
-                    size: 40.0,
-                  ),
+                  builder: (context) => const Icon(Icons.my_location,
+                      color: Colors.blue, size: 40.0),
                 ),
                 Marker(
                   point: widget.destination,
-                  builder: (context) => const Icon(
-                    Icons.location_on,
-                    color: Colors.red,
-                    size: 40.0,
-                  ),
+                  builder: (context) => const Icon(Icons.location_on,
+                      color: Colors.red, size: 40.0),
                 ),
-              ],
+              ]..addAll(incidentMarkers),
             ),
             PolylineLayer(
-              polylines: _generateRoutes(),
+              polylines: _generateRoutes() +
+                  trafficFlowPolylines, // Adding traffic flow polylines
             ),
           ],
         ),
         ZoomControls(mapController: mapController),
+        Positioned(
+          top: 50,
+          left: 20,
+          child: Material(
+            elevation: 6,
+            shape: const CircleBorder(),
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurple,
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+            ),
+          ),
+        ),
         Positioned(
           bottom: 20,
           left: 0,
@@ -185,9 +277,7 @@ class _RouteDrawerState extends State<RouteDrawer> {
                 items: transportModes.keys
                     .map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
+                      value: value, child: Text(value));
                 }).toList(),
                 onChanged: (String? newValue) {
                   if (newValue != null) {
@@ -197,32 +287,6 @@ class _RouteDrawerState extends State<RouteDrawer> {
                     });
                   }
                 },
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 50,
-          left: 20,
-          child: Material(
-            elevation: 6,
-            shape: const CircleBorder(),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).pop();
-              },
-              child: Container(
-                width: 56,
-                height: 56,
-                decoration: const BoxDecoration(
-                  color: Colors.deepPurple,
-                  shape: BoxShape.circle,
-                ),
-                child: const Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                  size: 30,
-                ),
               ),
             ),
           ),
