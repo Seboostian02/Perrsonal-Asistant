1dda036a4028e1dc292940e4ed9fccc30d840d15
Seboostian02 <antique_sebi@yahoo.ro>
2024-11-15 11:32:43 +0200

    updated color scheme

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // Primary Colors
-  static const Color primaryColor = Color(0xFF1976D2); // Blue 800
+  // Primary Colors (Deep Purple palette)
+  static const Color primaryColor = Color(0xFF512DA8); // Deep Purple 600
   static const Color primaryLightColor =
-      Color.fromARGB(255, 174, 208, 255); // Light blue
-  static const Color primaryDarkColor = Color(0xFF004BA0); // Dark blue
-  static const Color secondaryColor = Color(0xFF1565C0); // Blue 600
+      Color.fromARGB(255, 213, 187, 254); // Lighter Deep Purple
+  static const Color primaryDarkColor = Color(0xFF320B86); // Dark Deep Purple
+
+  // Secondary Colors (Purple palette)
+  static const Color secondaryColor = Color(0xFF673AB7); // Purple 600
   static const Color secondaryLightColor =
-      Color(0xFF5E92F3); // Light secondary blue
+      Color(0xFF9575CD); // Light secondary purple
   static const Color secondaryDarkColor =
-      Color(0xFF003C8F); // Dark secondary blue
+      Color(0xFF512DA8); // Dark secondary purple
+
   static const Color iconColor = Color.fromARGB(255, 255, 255, 255);
   static const Color iconPressedColor = const Color.fromRGBO(255, 235, 59, 1);
   static const Color textColor = Color.fromARGB(255, 255, 255, 255);
@@ -19,10 +22,8 @@ class AppColors {
   static const Color locationMarkerColor = Color.fromRGBO(244, 67, 54, 1);
 
   // Card and Background Colors
-
   static const Color cardColor = Colors.white; // White color for the card
-  static const Color footerColor = Color(0xFF6200EE);
-  // Divider Color
+  static const Color footerColor = Color(0xFF512DA8);
   static const Color dividerColor =
       Color(0xFFBDBDBD); // Light grey color for dividers
 }
